5ce3d73fa146fe4927fc9946198c021797c65837
RussellLVP <russell@sa-mp.nl>
2014-10-03 02:56:34 +0100

    Basic definition of the ArgumentValue structure.

--- src/test_runner/server/native_function_delegate_impl_internal.h
+++ src/test_runner/server/native_function_delegate_impl_internal.h
@@ -18,6 +18,9 @@
 
 #include <functional>
 #include <stdarg.h>
+#include <string>
+#include <tuple>
+#include <vector>
 
 class NativeFunctionDelegateImpl;
 
@@ -31,6 +34,33 @@ struct make_integer_sequence : make_integer_sequence<T, N - 1, N - 1, Is...> {};
 
 template<class T, int... Is>
 struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
+
+class ArgumentValue {
+ public:
+  explicit ArgumentValue(int value)
+      : type_(Integer) { primitive_.i = value; }
+  explicit ArgumentValue(double value)
+      : type_(Double) { primitive_.d = value; }
+  explicit ArgumentValue(std::string* value)
+      : type_(String) { primitive_.s = value; }
+
+ private:
+  enum Type { Integer, Double, String };
+  Type type_;
+
+  union {
+    int i;
+    double d;
+    std::string* s;
+
+  } primitive_;
+};
+
+template <typename... Arguments>
+struct ArgumentForwarder {
+ public:
+
+};
 
 }  // namespace internal
 
@@ -49,13 +79,15 @@ class NativeFunction : public NativeFunctionBase {
       : method_(method) {}
 
   virtual ~NativeFunction() override {}
-  virtual int Invoke(NativeFunctionDelegateImpl* instance, va_list arguments) const override {
-    return InvokeImpl(instance, arguments, internal::make_integer_sequence<unsigned int, sizeof...(Arguments)>());
+  virtual int Invoke(NativeFunctionDelegateImpl* instance, va_list argument_list) const override {
+    return InvokeImpl(instance, argument_list, internal::make_integer_sequence<unsigned int, sizeof...(Arguments)>());
   }
 
  private:
   template <unsigned int... Indices>
-  int InvokeImpl(NativeFunctionDelegateImpl* instance, va_list arguments, internal::integer_sequence<unsigned int, Indices...>) const {
+  int InvokeImpl(NativeFunctionDelegateImpl* instance, va_list argument_list, internal::integer_sequence<unsigned int, Indices...>) const {
+    std::vector<internal::ArgumentValue> arguments;
+
     return 0;
   }
 
