a23bd4b10cf178a43914a51ab24eaf5b9e5dcd43
RussellLVP <russell@sa-mp.nl>
2014-10-16 23:36:35 +0100

    Write tests for the Timer functionality
    Yay for TDD.

--- src/playground/services/timers/timer_unittest.cpp
+++ src/playground/services/timers/timer_unittest.cpp
@@ -15,8 +15,127 @@
 
 #include "playground/services/timers/timer.h"
 
-#include "gtest/gtest.h"
+#include <thread>
 
-TEST(TimerTest, DummyTest) {
-  EXPECT_EQ(true, !false);
+#include "gtest/gtest.h"
+#include "playground/services/timers/thread_timer_manager.h"
+
+// Timers require a clean run-time of the server environment, so before each test we clear out the
+// timer manager for the thread these tests are being ran on.
+class TimerTest : public testing::Test {
+ public:
+  virtual void SetUp() override {
+    primary_invocation_count_ = 0;
+    secondary_invocation_count_ = 0;
+
+    // This completely clears out the state of the thread timer manager.
+    ThreadTimerManager::InstanceForThread()->Shutdown();
+  }
+
+  void PrimaryCounter() { ++primary_invocation_count_; }
+  void SecondaryCounter() { ++secondary_invocation_count_; }
+
+  unsigned int primary_invocation_count() const { return primary_invocation_count_; }
+  unsigned int secondary_invocation_count() const { return secondary_invocation_count_; }
+
+ private:
+  unsigned int primary_invocation_count_;
+  unsigned int secondary_invocation_count_;
+};
+
+// -------------------------------------------------------------------------------------------------
+
+TEST_F(TimerTest, ImmediateOneShotTimer) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  EXPECT_FALSE(timer.IsActive());
+  timer.Start(TimeSpan::FromMicroseconds(0), false);
+  EXPECT_FALSE(timer.is_repeating());
+  EXPECT_TRUE(timer.IsActive());
+
+  EXPECT_EQ(0, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(1, primary_invocation_count());
+}
+
+TEST_F(TimerTest, ImmediateRepeatedTimer) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  timer.Start(TimeSpan::FromMicroseconds(0), true);
+  EXPECT_TRUE(timer.is_repeating());
+
+  EXPECT_EQ(0, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(1, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(2, primary_invocation_count());
+}
+
+TEST_F(TimerTest, DelayedOneShotTimer) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  timer.Start(TimeSpan::FromMilliseconds(4), false);
+
+  EXPECT_EQ(0, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(0, primary_invocation_count());
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(1, primary_invocation_count());
+}
+
+TEST_F(TimerTest, DelayedRepeatedTimer) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  timer.Start(TimeSpan::FromMilliseconds(2), true);
+
+  EXPECT_EQ(0, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(0, primary_invocation_count());
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(3));
+
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(1, primary_invocation_count());
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(1, primary_invocation_count());
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(3));
+
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+  EXPECT_EQ(2, primary_invocation_count());
+}
+
+TEST_F(TimerTest, MultipleTimers) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  timer.Start(TimeSpan::FromMicroseconds(0), true);
+
+  Timer delayed_timer(std::bind(&TimerTest::SecondaryCounter, this));
+  delayed_timer.Start(TimeSpan::FromMilliseconds(2), true);
+
+  EXPECT_EQ(0, primary_invocation_count());
+  EXPECT_EQ(0, secondary_invocation_count());
+
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+
+  EXPECT_EQ(1, primary_invocation_count());
+  EXPECT_EQ(0, secondary_invocation_count());
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(3));
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+
+  EXPECT_EQ(2, primary_invocation_count());
+  EXPECT_EQ(1, secondary_invocation_count());
+
+  ThreadTimerManager::InstanceForThread()->ProcessTimers();
+
+  EXPECT_EQ(3, primary_invocation_count());
+  EXPECT_EQ(1, secondary_invocation_count());
+}
+
+TEST_F(TimerTest, OrphanedTimer) {
+  Timer timer(std::bind(&TimerTest::PrimaryCounter, this));
+  timer.Start(TimeSpan::FromMicroseconds(0), true);
+
+  EXPECT_EQ(timer.is_orphan(), false);
+  ThreadTimerManager::InstanceForThread()->Shutdown();
+  EXPECT_EQ(timer.is_orphan(), true);
 }
